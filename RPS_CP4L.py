fcea2635b584a2b22d915ca57999762f0c2bc2c1
Jeagle101 <88680849+Jeagle101@users.noreply.github.com>
2023-03-28 19:35:33 -0400

    Complete rewrite, q-table implement

--- RPS_CP4L.py
+++ RPS_CP4L.py
@@ -1,66 +1,72 @@
-# Code below was taken from ChatGPT, believe it or not.
 import random
 
+# Set up the Q-table with initial values
+q_table = {
+    ('R', 'R'): 0.5,
+    ('R', 'P'): 0.5,
+    ('R', 'S'): 0.5,
+    ('P', 'R'): 0.5,
+    ('P', 'P'): 0.5,
+    ('P', 'S'): 0.5,
+    ('S', 'R'): 0.5,
+    ('S', 'P'): 0.5,
+    ('S', 'S'): 0.5,
+}
 
-class Bot:
-    def __init__(self):
-        self.history = {'R': 0, 'P': 0, 'S': 0}
-        self.move = ''
+# Define the learning rate and discount factor
+learning_rate = 0.1
+discount_factor = 0.9
 
-    def update_history(self, opponent_move):
-        self.history[opponent_move] += 1
+# Define a function to select an action based on the Q-table values
+def select_action(state):
+    if random.uniform(0, 1) < 0.1:
+        # Choose a random action with probability 0.1
+        return random.choice(['R', 'P', 'S'])
+    else:
+        # Choose the action with the highest Q-value for the current state
+        return max(q_table[state], key=q_table[state].get)
 
-    def choose_move(self):
-        total_moves = sum(self.history.values())
-        if total_moves == 0:
-            self.move = random.choice(['R', 'P', 'S'])
+# Define a function to update the Q-table based on the results of a game
+def update_q_table(state, action, reward, next_state):
+    old_q_value = q_table[state][action]
+    next_max_q_value = max(q_table[next_state].values())
+    new_q_value = (1 - learning_rate) * old_q_value + learning_rate * (reward + discount_factor * next_max_q_value)
+    q_table[state][action] = new_q_value
+
+# Define a function to play a game against the opponent and update the Q-table
+def play_game():
+    state = ('R', 'R')
+    total_reward = 0
+
+    while True:
+        # Choose an action based on the current state
+        action = select_action(state)
+
+        # Get the opponent's move
+        opponent_move = input("Opponent move (R/P/S): ").upper()
+
+        # Determine the reward based on the outcome of the game
+        if action == opponent_move:
+            reward = 0
+        elif (action == 'R' and opponent_move == 'S') or (action == 'P' and opponent_move == 'R') or (action == 'S' and opponent_move == 'P'):
+            reward = 1
         else:
-            opponent_moves = list(self.history.keys())
-            opponent_frequency = list(self.history.values())
-            if opponent_frequency.count(max(opponent_frequency)) > 1:
-                moves = [i for i in range(3) if opponent_frequency[i] == max(opponent_frequency)]
-                self.move = opponent_moves[random.choice(moves)]
-            else:
-                self.move = opponent_moves[opponent_frequency.index(max(opponent_frequency))]
-        return self.move
+            reward = -1
 
-    def learn(self, opponent_move):
-        self.update_history(opponent_move)
+        # Update the total reward and the Q-table
+        total_reward += reward
+        next_state = (action, opponent_move)
+        update_q_table(state, action, reward, next_state)
+        state = next_state
 
+        # Print the current Q-table and total reward
+        print(q_table)
+        print("Total reward:", total_reward)
 
-class Game:
-    def __init__(self, player1, player2):
-        self.player1 = player1
-        self.player2 = player2
-        self.player1_score = 0
-        self.player2_score = 0
+        # Ask the player if they want to continue playing
+        choice = input("Continue playing? (y/n): ")
+        if choice.lower() == 'n':
+            break
 
-    def play(self, rounds):
-        for i in range(rounds):
-            player1_move = self.player1.choose_move()
-            player2_move = self.player2.choose_move()
-
-            print(f"Player 1: {player1_move}")
-            print(f"Player 2: {player2_move}")
-
-            if player1_move == player2_move:
-                print("Tie!")
-            elif (player1_move == "R" and player2_move == "S") or (player1_move == "P" and player2_move == "R") or (
-                    player1_move == "S" and player2_move == "P"):
-                print("Player 1 wins!")
-                self.player1_score += 1
-                self.player1.learn(player2_move)
-                self.player2.learn(player1_move)
-            else:
-                print("Player 2 wins!")
-                self.player2_score += 1
-                self.player2.learn(player1_move)
-                self.player1.learn(player2_move)
-
-            print(f"Score: Player 1: {self.player1_score}, Player 2: {self.player2_score}\n")
-
-
-bot1 = Bot()
-bot2 = Bot()
-game = Game(bot1, bot2)
-game.play(10000)
+# Play the game
+play_game()
